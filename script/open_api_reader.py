c402c0f1691216a01ee47195508a1d2ce9b486b5
Lee Kyusang <35690424+qsang86@users.noreply.github.com>
2021-01-04 23:36:40 +0800

    Initial commit

---
+++ script/open_api_reader.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+
+def open_api_reader():
+    open_api_key = 'VwUNnuPS5treeoYbtM8R2Cw89HPvmvApECDi28RBhaY4z2eFUCx2WjEXUm1w%2B6iibQjSA5FxoeCMm3Pj5YDlhA%3D%3D'
+    params = '&pageNo=1&numOfRows=10&startCreateDt=20200310&endCreateDt=20200315'
+
+    #url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?ServiceKey=' + open_api_key + params
+    #end_point = 'http://openapi.data.go.kr/openapi/service/rest/Covid19'
+    open_url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19NatInfStateJson?serviceKey=VwUNnuPS5treeoYbtM8R2Cw89HPvmvApECDi28RBhaY4z2eFUCx2WjEXUm1w%2B6iibQjSA5FxoeCMm3Pj5YDlhA%3D%3D&pageNo=1&numOfRows=10&startCreateDt=20200310&endCreateDt=20200414'
+
+
+    res = requests.get(open_url).text.encode('utf-8')
+    soup = BeautifulSoup(res, 'lxml-xml')
+
+    rows = soup.findAll('item')
+    columns = rows[0].find_all()
+
+    rowlist = []
+    collist = []
+    tlist = []
+
+    for i in range(0, len(rows)):
+        columns = rows[i].find_all()
+        for j in range(0, len(columns)):
+            if i == 0:
+                tlist.append(columns[j].name)
+            col_ = columns[j].text
+            collist.append(col_)
+        rowlist.append(collist)
+        collist = []
+
+    result_df = pd.DataFrame(rowlist, columns=tlist)
+    print('number of rows: ', len(result_df.index))
+
+    return result_df
